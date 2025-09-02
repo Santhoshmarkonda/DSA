ce60ef9a0763390a085efb7d5260d5efd194a710
Santhoshmarkonda <santhoshmarkonda786@gmail.com>
2025-09-02 23:39:58 +0530

    day-9

--- day9/Pyramid.java
+++ day9/Pyramid.java
@@ -7,11 +7,11 @@ public class Pyramid {
 
         for (int i = 1; i <= n; i++) {
             for (int space = 1; space <= n - i; space++) {
-                System.out.print("  ");
+                System.out.print(" ");
             }
             
             for (int j = 1; j <= i; j++) {
-                System.out.print("*   ");  
+                System.out.print("* ");  
             }
 
             System.out.println();
