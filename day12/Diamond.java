eb3ec6ededc0fa8b38d923fe6750d31be8d46973
Santhoshmarkonda <santhoshmarkonda786@gmail.com>
2025-09-07 18:14:26 +0530

    day-12

---
+++ day12/Diamond.java
@@ -0,0 +1,34 @@
+package ps.day12;
+
+public class Diamond {
+
+	public static void getPattern(int n) {
+		// 1st Half
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n - i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= (2 * i) - 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+		// 2nd Half
+		for (int i = n; i >= 1; i--) {
+			for (int j = 1; j <= n - i; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= (2 * i) - 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		int n = 4;
+		getPattern(n);
+	}
+
+}
---
+++ day12/InvertedHalfPyramidWithNumbers.java
@@ -0,0 +1,17 @@
+package ps.day12;
+
+public class InvertedHalfPyramidWithNumbers {
+	
+	public static void getPattern(int n) {
+		for(int i = 1; i<= n;i++) {
+			for(int j = 1; j <= n-i+1;j++) {
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+	}
+    public static void main(String[] args) {
+		int n = 5;
+		getPattern(n);
+	}
+}
