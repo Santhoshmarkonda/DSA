7e987230c83d04a53bf2a740ed766a33865082e7
Santhoshmarkonda <santhoshmarkonda786@gmail.com>
2025-09-23 22:36:32 +0530

    Day-25

---
+++ day25/BinaryNumberSwap.java
@@ -0,0 +1,17 @@
+package ps.day25;
+
+public class BinaryNumberSwap {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+
+		a = a ^ b;
+		b = a ^ b;
+		a = a ^ b;
+
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
